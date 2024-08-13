76eda554dbb865b39cb7e73cea90cbe0a42e57c4
Somaie Kaveh <somaie.kaveh@gmail.com>
2024-08-13 16:20:31 +0330

    handle multiple clicks on images

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ var game = new Vue({
 	data: {
 		firstClick: false,
 		firstCardId:'',
-		condition: false,
+		isProssesing: false,
 		pics: [
 			{id:0, source: './picgame/basket.jpg', save: false},
 			{id:1, source: './picgame/basket.jpg', save: false},
@@ -21,7 +21,8 @@ var game = new Vue({
 	},
 	methods:{
 		showImage: function(cardId) {
-			if(this.pics[cardId].save === true){ return; }
+			if(this.pics[cardId].save === true || this.firstCardId === cardId || this.isProssesing === true ){ return; }
+			this.isProssesing = true;
 			document.getElementById(`card-${cardId}`).style.transform = 'rotateY(180deg)';
 			this.pics[cardId].source = `./picgame/furit${cardId > 5 ? cardId-6 : cardId}.jpg`;
 			this.firstClick = !this.firstClick;
@@ -29,8 +30,10 @@ var game = new Vue({
 				var firstCard = this.pics[this.firstCardId].source;
 				var secondCard =  this.pics[cardId].source;
 				if(firstCard === secondCard){
-					this.pics[cardId].save =true,
-					this.pics[this.firstCardId].save = true
+					this.pics[cardId].save = true;
+					this.pics[this.firstCardId].save = true;
+					this.firstCardId='';
+					this.isProssesing = false;
 				} else if( secondCard != firstCard) {
 					setTimeout(() => {
 						document.getElementById(`card-${cardId}`).style.transform = 'rotateY(90deg)';
@@ -40,12 +43,15 @@ var game = new Vue({
 							this.pics[this.firstCardId].source = './picgame/basket.jpg';
 							document.getElementById(`card-${cardId}`).style.transform = 'rotateY(0deg)';
 							document.getElementById(`card-${this.firstCardId}`).style.transform = 'rotateY(0deg)';
+							this.firstCardId = '';
+							this.isProssesing = false;
 						}, 100);
 					}, 500);
 				}
 			} else {
 				this.firstCardId = cardId;
+				this.isProssesing = false;
 			}
-		}
+		},
 	}
 })
