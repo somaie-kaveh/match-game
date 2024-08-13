899308cdf3c4f96460761111100f284af61e5889
Somaie Kaveh <somaie.kaveh@gmail.com>
2024-08-13 13:34:08 +0330

    make the matched images unclickable

--- game.js
+++ game.js
@@ -3,30 +3,35 @@ var game = new Vue({
 	data: {
 		firstClick: false,
 		firstCardId:'',
+		condition: false,
 		pics: [
-			{id:0, source: './picgame/basket.jpg'},
-			{id:1, source: './picgame/basket.jpg'},
-			{id:2, source: './picgame/basket.jpg'},
-			{id:3, source: './picgame/basket.jpg'},
-			{id:4, source: './picgame/basket.jpg'},
-			{id:5, source: './picgame/basket.jpg'},
-			{id:6, source: './picgame/basket.jpg'},
-			{id:7, source: './picgame/basket.jpg'},
-			{id:8, source: './picgame/basket.jpg'},
-			{id:9, source: './picgame/basket.jpg'},
-			{id:10, source: './picgame/basket.jpg'},
-			{id:11, source: './picgame/basket.jpg'},
+			{id:0, source: './picgame/basket.jpg', save: false},
+			{id:1, source: './picgame/basket.jpg', save: false},
+			{id:2, source: './picgame/basket.jpg', save: false},
+			{id:3, source: './picgame/basket.jpg', save: false},
+			{id:4, source: './picgame/basket.jpg', save: false},
+			{id:5, source: './picgame/basket.jpg', save: false},
+			{id:6, source: './picgame/basket.jpg', save: false},
+			{id:7, source: './picgame/basket.jpg', save: false},
+			{id:8, source: './picgame/basket.jpg', save: false},
+			{id:9, source: './picgame/basket.jpg', save: false},
+			{id:10, source: './picgame/basket.jpg', save: false},
+			{id:11, source: './picgame/basket.jpg', save: false},
 		]
 	},
 	methods:{
 		showImage: function(cardId) {
+			if(this.pics[cardId].save === true){ return; }
 			document.getElementById(`card-${cardId}`).style.transform = 'rotateY(180deg)';
 			this.pics[cardId].source = `./picgame/furit${cardId > 5 ? cardId-6 : cardId}.jpg`;
 			this.firstClick = !this.firstClick;
 			if (this.firstClick === false) {
 				var firstCard = this.pics[this.firstCardId].source;
 				var secondCard =  this.pics[cardId].source;
-				if( secondCard != firstCard) {
+				if(firstCard === secondCard){
+					this.pics[cardId].save =true,
+					this.pics[this.firstCardId].save = true
+				} else if( secondCard != firstCard) {
 					setTimeout(() => {
 						document.getElementById(`card-${cardId}`).style.transform = 'rotateY(90deg)';
 						document.getElementById(`card-${this.firstCardId}`).style.transform = 'rotateY(90deg)';
